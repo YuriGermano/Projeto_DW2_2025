542af5067e31a4b2c2daa279db0efdc8b7784ed8
EmanuelBSM <emanuelbsmartins@gmail.com>
2025-08-06 08:48:44 -0300

    Update carros_info.jsx

--- Trabalho_DW/src/componentes/carros_info.jsx
+++ Trabalho_DW/src/componentes/carros_info.jsx
@@ -1,3 +1,7 @@
 // const carros = [
 //     {id: 1, nome: "FERRARI 458 ITALIA SPIDER", taxaDiaria: 1000.00, precoEspecial: 800.00, imagem: "../../public/imagens/Ferrari_1.png"}
 // ]
+
+
+
+// Ferrari 458 Italia Spider, Lamborghini Aventador SVJ, Mercedes-Benz s550, BMW X7, Mclaren Senna, Cadillac Escalade, Range Rover Sport, Dodge Ram 3500, Porsche 911 GT3 RS.

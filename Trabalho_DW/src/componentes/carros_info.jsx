bcdad0e31795bf6361be3e086fb5efa27820bbc9
EmanuelBSM <emanuelbsmartins@gmail.com>
2025-08-15 16:49:05 -0300

    Quase no fim dos links

--- Trabalho_DW/src/componentes/carros_info.jsx
+++ Trabalho_DW/src/componentes/carros_info.jsx
@@ -1,7 +1,108 @@
-// const carros = [
-//     {id: 1, nome: "FERRARI 458 ITALIA SPIDER", taxaDiaria: 1000.00, precoEspecial: 800.00, imagem: "../../public/imagens/Ferrari_1.png"}
-// ]
+// src/data/carros.js
+const carros = [
+  {
+    id: 1,
+    nome: "Ferrari 458 Spider",
+    potencia: "570 CV @ 9.000 rpm",
+    transmissao: "F1 Dual-clutch transmission, 7 velocidades",
+    motor: "4.497 cm³, V8 90°, injeção direta, cárter seco",
+    aceleracao: "< 3,4 segundos",
+    tracao: "RWD",
+    preco: "$875 (R$ 4.375)",
+    imagem: [
+        "/imagens/Ferrari_1.png",
+        "/imagens/Ferrari_2.png",
+        "/imagens/Ferrari_3.png"
+    ]
+  },
+  {
+    id: 2,
+    nome: "Lamborghini Aventador SVJ",
+    potencia: "770 CV @ 8.500 rpm",
+    transmissao: "Automático, 7 marchas",
+    motor: "6,5 L V12 aspirado",
+    aceleracao: "2,8–2,9 segundos",
+    tracao: "AWD",
+    preco: "$2.098 (R$ 10.490)",
+    imagem: "/imagens/Lambo_1.png"
+  },
+  {
+    id: 3,
+    nome: "Mercedes-Benz S550",
+    potencia: "~449 CV",
+    transmissao: "Automática, 7 marchas",
+    motor: "V8 biturbo ou aspirado (4,7 L ou 4,0 L)",
+    aceleracao: "4,5–5,4 segundos",
+    tracao: "RWD / AWD",
+    preco: "$425 (R$ 2.125)",
+    imagem: "/imagens/mercedes-s550.jpg"
+  },
+  {
+    id: 4,
+    nome: "BMW X7",
+    potencia: "381 CV",
+    transmissao: "Automática, 8 velocidades TwinPower Turbo",
+    motor: "6 cilindros em linha, TwinPower Turbo",
+    aceleracao: "5,8 segundos",
+    tracao: "AWD (xDrive)",
+    preco: "$370 (R$ 1.850)",
+    imagem: "/imagens/bmw-x7.jpg"
+  },
+  {
+    id: 5,
+    nome: "Porsche 911 GT3 RS",
+    potencia: "518 CV",
+    transmissao: "PDK dupla embreagem",
+    motor: "4.0 L flat-6 aspirado",
+    aceleracao: "3,2 segundos",
+    tracao: "RWD",
+    preco: "$1.179 (R$ 5.900)",
+    imagem: "/imagens/porsche-911.jpg"
+  },
+  {
+    id: 6,
+    nome: "Range Rover Sport SV",
+    potencia: "635 PS",
+    transmissao: "Automática, 8 velocidades",
+    motor: "4.4 L V8 twin-turbo MHEV",
+    aceleracao: "3,9 segundos",
+    tracao: "AWD",
+    preco: "$349 (R$ 1.745)",
+    imagem: "/imagens/range-rover-sv.jpg"
+  },
+  {
+    id: 7,
+    nome: "Dodge RAM 3500",
+    potencia: "370–405 CV",
+    transmissao: "Automática, 8 marchas",
+    motor: "V8 HEMI 6,4 L ou seis em linha diesel turbo",
+    aceleracao: "8–8,8 segundos",
+    tracao: "RWD / 4WD",
+    preco: "R$ 1.500",
+    imagem: "/imagens/dodge-ram.jpg"
+  },
+  {
+    id: 8,
+    nome: "McLaren Senna",
+    potencia: "789 CV",
+    transmissao: "Automática DCT, 7 marchas",
+    motor: "4.0 L V8 twin-turbo, dry-sump",
+    aceleracao: "2,8 segundos",
+    tracao: "RWD",
+    preco: "Preço sob consulta",
+    imagem: "/imagens/mclaren-senna.jpg"
+  },
+  {
+    id: 9,
+    nome: "Cadillac Escalade-V",
+    potencia: "692 CV",
+    transmissao: "Automática, 10 velocidades",
+    motor: "6.2 L V8 supercharged",
+    aceleracao: "4,4 segundos",
+    tracao: "AWD",
+    preco: "$275 (R$ 1.375)",
+    imagem: "/imagens/cadillac-escalade.jpg"
+  }
+];
 
-
-
-// Ferrari 458 Italia Spider, Lamborghini Aventador SVJ, Mercedes-Benz s550, BMW X7, Mclaren Senna, Cadillac Escalade, Range Rover Sport, Dodge Ram 3500, Porsche 911 GT3 RS.
+export default carros;
--- Trabalho_DW/src/paginas/index.jsx
+++ Trabalho_DW/src/paginas/index.jsx
@@ -1,152 +1,63 @@
-import Footer from '../componentes/footer.jsx'
-import '../style/index.css'
+import Footer from '../componentes/footer.jsx';
+import '../style/index.css';
 import { Link } from "react-router-dom";
 import carro_info from '../componentes/carros_info.jsx';
 
-
 export default function Main() {
-    return (
-        <div id='div_mae'>
-            <section id="pagina_inicial" 
-            style={{backgroundImage: "url('/imagens/imgfundo.png')",
-                backgroundSize: "cover",
-                width: "1530px",
-                marginLeft: "-10%", 
-                marginTop: "-32px",
-
-            }}>
-                <div id="quadrado_inicial"></div>
-                <header>
-                    <nav className="navbar">
-                        <div className="logo">
-                            <img src="/imagens/logo.png" alt="logo_site" />
-                        </div>
-                        <div className="escritas_header">
-                            <p>Home</p>
-                            <a href="#carros"><p>Carros</p></a>
-                            <p>Sobre Nós</p>
-                            <p>Contato</p>
-                        </div>
-                    </nav>
-                </header>
-                <div id="frase_inicial">
-                    <p>Seu <strong>Destino</strong> <br />Começa<br /> Com a <strong>Chave</strong><br /> Certa</p>
-                </div>
-                <div id="retangulo_inicial"></div>
-            </section>
-
-
-            {carro_info.map((carros) => {      {/*  Percorre o array dos carros, mapeando eles, percorre e renderiza cada item da lista*/}
-            <section className="carros">
-                <div className="grid_container" id='carros'>
-                    <Link to={`/carro/${carros.id}`}>        {/* Cria o Link personalizado para cada carro */}
-                        <div key={carros.id} className="carro">         {/*Key identifica cada item*/}
-                            <div className="text">
-                                <h1>FERRARI 458 ITALIA SPIDER</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (1).png" />
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro">
-                            <div className="text">
-                                <h1>LAMBORGHINI AVENTADOR SVJ</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (4).png" />
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro"> 
-                            <div className="text">
-                                <h1>MCLAREN 720s</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (8).jpeg" />
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro">
-                            <div className="text">
-                                <h1>CADILLAC ESCALADE</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (3).jpg" />
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro">
-                            <div className="text">
-                                <h1>DOGDE RAM 3500</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (2).jpeg" />
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                    <div key={carros.id} className="carro">
-                        <div className="text">
-                            <h1>Mercedes-AMG GT 63</h1>
-                            <p>Taxa Diária: R$ 1000.00</p>
-                            <p>Preço Especial: R$ 800.00</p>
-                        </div>
-                        <img src="/imagens/image (5).png" />
-                    </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro">
-                            <div className="text">
-                                <h1>RANGER ROVER SPORT</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (10).jpg"/>
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro">
-                            <div className="text">
-                                <h1>PORSCHE 911 GT3 RS</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (9).jpg"/>
-                        </div>
-                    </Link>
-
-                    <Link to={`/carro/${carros.id}`}>
-                        <div key={carros.id} className="carro">
-                            <div className="text">
-                                <h1>BMW X7</h1>
-                                <p>Taxa Diária: R$ 1000.00</p>
-                                <p>Preço Especial: R$ 800.00</p>
-                            </div>
-                            <img src="/imagens/image (7).jpg" alt=""/>
-                        </div>
-                    </Link>
-                </div>
-                
-            </section>})};
-
-
-            
-            <footer>
-                <Footer />
-            </footer>
-
+  return (
+    <div id='div_mae'>
+      {/* Seção inicial */}
+      <section 
+        id="pagina_inicial"
+        style={{
+          backgroundImage: "url('/imagens/imgfundo.png')",
+          backgroundSize: "cover",
+          width: "1530px",
+          marginLeft: "-10%", 
+          marginTop: "-32px",
+        }}
+      >
+        <div id="quadrado_inicial"></div>
+        <header>
+          <nav className="navbar">
+            <div className="logo">
+              <img src="/imagens/logo.png" alt="logo_site" />
+            </div>
+            <div className="escritas_header">
+              <p>Home</p>
+              <a href="#carros"><p>Carros</p></a>
+              <p>Sobre Nós</p>
+              <p>Contato</p>
+            </div>
+          </nav>
+        </header>
+        <div id="frase_inicial">
+          <p>Seu <strong>Destino</strong> <br />Começa<br /> Com a <strong>Chave</strong><br /> Certa</p>
         </div>
-    )
+        <div id="retangulo_inicial"></div>
+      </section>
+
+      {/* Lista de carros */}
+      {carro_info.map((carro) => (
+        <section className="carros" key={carro.id} id="carros">   {/*  key={carro.id} → identifica unicamente cada item do .map() */}
+          <div className="grid_container">
+            <Link to={`/carro/${carro.id}`}>
+              <div className="carro">
+                <div className="text">
+                  <h1>{carro.nome}</h1>
+                  <p>Preço: {carro.preco}</p>
+                </div>
+                <img src={carro.imagem} alt={carro.nome} />
+              </div>
+            </Link>
+          </div>
+        </section>
+      ))}
+
+      {/* Footer */}
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
